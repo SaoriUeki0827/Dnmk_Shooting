026568504e51563cee970c1e77de10af9f9c4e36
matsuzawa <kbc15b18@stu.kawahara.ac.jp>
2018-01-25 11:00:54 +0900

    send

---
+++ Shooting/Assets/script/Matsuzawa/Stage01/Background.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    //スクロールするスピード
+    public float speed = 0.1f;
+
+	void Update ()
+    {
+        //時間によってＹの値が０から１に変化していく。１になったら０に戻り、繰り返す。
+        float y = Mathf.Repeat(Time.time * speed, 1);
+
+        //Ｙの値がずれていくオフセットを作成
+        Vector2 offset = new Vector2(0, y);
+
+        //マテリアルにオフセットを設定する
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
